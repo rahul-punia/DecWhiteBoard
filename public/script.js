09ae362e5303f2a4d8dc8c59cabef558ace8493e
rahul-punia <rahulpunia1011@gmail.com>
2020-06-19 17:04:28 +0530

    update css

--- public/script.js
+++ public/script.js
@@ -8,6 +8,7 @@ board.height = window.innerHeight;
 board.width = window.innerWidth;
 // canvasRenderingContext2d=> tool
 const ctx = board.getContext("2d");
+//concept
 // ctx.fillRect(0, 0, window.innerWidth / 2, window.innerHeight / 2);
 // ctx.fillStyle = "red";
 // // initialX,initialY,finalX,finalY
@@ -17,13 +18,21 @@ const ctx = board.getContext("2d");
 // ctx.strokeRect(0, 0, window.innerWidth / 2, window.innerHeight / 2);
 ctx.strokeStyle = "blue";
 ctx.imageSmoothingEnabled = true
+
+//set size of pencil
 const input = document.querySelector("#pen-size");
 ctx.lineWidth = input.value;
 input.addEventListener("change", function (e) {
     ctx.lineWidth = input.value;
-
 })
-// ctx.lineWidth=2;
+//set size of eraser
+const inputEsize = document.querySelector("#eraser-size");
+ctx.lineWidth = inputEsize.value;
+inputEsize.addEventListener("change", function (e) {
+    ctx.lineWidth = inputEsize.value;
+})
+
+
 let Activetool = "pencil";
 const pencilOptions = document.querySelector(".pencil");
 const eraserOptions = document.querySelector(".eraser");
